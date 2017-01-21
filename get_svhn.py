0c2f3cdd877ab88a159d9b57b391421cf93dd880
Rob-M-F <rob.m.fitch@gmail.com>
2017-01-20 20:17:11 -0500

    Refactored to allow use of either SVHN dataset
    Factored common elements out of SVHN dataset processing and included an
    option to choose which dataset to work with.

--- get_svhn.py
+++ get_svhn.py
@@ -4,59 +4,74 @@ Created on Tue Jan 17 19:20:46 2017
 
 @author: RMFit
 """
+# Imports
+import os
+import numpy as np
+import scipy.io
+import sys
+import tarfile
+from six.moves.urllib.request import urlretrieve
 
-def make_composite_dataset():
-    # Imports
-    import os
-    import numpy as np
-    import scipy.io
-    from six.moves.urllib.request import urlretrieve
-    
-    url = 'http://ufldl.stanford.edu/housenumbers/'
-    np.random.seed(1)
-            
-    def maybe_download(filename, expected_bytes, force=False):
+np.random.seed(1)
+
+def maybe_download(url, filename, expected_bytes, force=False):
       """Download a file if not present, and make sure it's the right size."""
       if force or not os.path.exists(filename):
         print('Attempting to download:', filename) 
         filename, _ = urlretrieve(url + filename, filename)
         print('\nDownload Complete!')
       statinfo = os.stat(filename)
+      print(statinfo.st_size)
       if statinfo.st_size == expected_bytes:
         pass
       else:
         raise Exception(
           'Failed to verify ' + filename + '. Can you get to it with a browser?')
       return filename
-    
-    def load_data(filename):
-        data_dict = scipy.io.loadmat(filename)
-        dataset = data_dict
-        return dataset
 
-    def get_data_labels(dataset):
-        working_data = np.swapaxes(dataset['X'], 2, 3)
-        working_data = np.swapaxes(working_data, 1, 2)
-        working_data = np.swapaxes(working_data, 0, 1)
-        working_labels = dataset['y']
-        return working_data, working_labels
+def maybe_extract(filename, force=False):
+      root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+      if os.path.isdir(root) and not force:
+        # You may override by setting force=True.
+        pass
+      else:
+        print('Extracting data for %s. This may take a while. Please wait.' % root)
+        tar = tarfile.open(filename)
+        sys.stdout.flush()
+        tar.extractall()
+        tar.close()
+      data_folders = [
+        os.path.join(root, d) for d in sorted(os.listdir(root))
+        if os.path.isdir(os.path.join(root, d)) and (len(d) == 1)]
+      return data_folders      
+      
+def get_svhn_data_labels(dataset):
+    working_data = np.swapaxes(dataset['X'], 2, 3)
+    working_data = np.swapaxes(working_data, 1, 2)
+    working_data = np.swapaxes(working_data, 0, 1)
+    working_labels = dataset['y']
+    return working_data, working_labels
+
+def small_svhn_dataset():
+    
+    svhn_url = 'http://ufldl.stanford.edu/housenumbers/'
 
     print('Starting')
-    train_filename = maybe_download('train_32x32.mat', 182040794)
-    test_filename = maybe_download('test_32x32.mat', 64275384)
-    extra_filename = maybe_download('extra_32x32.mat', 1329278602)
+    train_filename = maybe_download(svhn_url, 'train_32x32.mat', 182040794)
+    test_filename = maybe_download(svhn_url, 'test_32x32.mat', 64275384)
+    extra_filename = maybe_download(svhn_url, 'extra_32x32.mat', 1329278602)
     print('Download Complete')
-   
-    train_dataset = load_data(train_filename)
-    test_dataset = load_data(test_filename)
-    extra_dataset = load_data(extra_filename)
+
+    train_dataset = scipy.io.loadmat(train_filename)
+    test_dataset = scipy.io.loadmat(test_filename)
+    extra_dataset = scipy.io.loadmat(extra_filename)
     print('Loading Complete')
    
-    train_dataset, train_labels = get_data_labels(train_dataset)
-    extra_dataset, extra_labels = get_data_labels(extra_dataset)
+    train_dataset, train_labels = get_svhn_data_labels(train_dataset)
+    extra_dataset, extra_labels = get_svhn_data_labels(extra_dataset)
     extra_dataset = np.append(extra_dataset, train_dataset, axis=0)
     extra_labels = np.append(extra_labels, train_labels, axis=0)
-    test_dataset, test_labels = get_data_labels(test_dataset)
+    test_dataset, test_labels = get_svhn_data_labels(test_dataset)
     print('Prepared')  
     
     dataset = {}
@@ -69,8 +84,52 @@ def make_composite_dataset():
     print('Dataset Built')    
 
     return dataset
+
+def big_svhn_dataset():
+   
+    svhn_url = 'http://ufldl.stanford.edu/housenumbers/'
+
+    print('Starting')
+    train_filename = maybe_download(svhn_url, 'train.tar.gz', 404141560)
+    test_filename = maybe_download(svhn_url, 'test.tar.gz', 276555967)
+    extra_filename = maybe_download(svhn_url, 'extra.tar.gz', 1955489752)
+    print('Download Complete')
+
+    train_folders = maybe_extract(train_filename)
+    test_folders = maybe_extract(test_filename)
+    extra_folders = maybe_extract(extra_filename)
+    print('Extract Complete')
+
+    train_dataset = scipy.io.loadmat(train_filename)
+    test_dataset = scipy.io.loadmat(test_filename)
+    extra_dataset = scipy.io.loadmat(extra_filename)
+    print('Loading Complete')
+   
+    train_dataset, train_labels = get_svhn_data_labels(train_dataset)
+    extra_dataset, extra_labels = get_svhn_data_labels(extra_dataset)
+    extra_dataset = np.append(extra_dataset, train_dataset, axis=0)
+    extra_labels = np.append(extra_labels, train_labels, axis=0)
+    test_dataset, test_labels = get_svhn_data_labels(test_dataset)
+    print('Prepared')  
     
-#svhn_data = make_composite_dataset()
+    dataset = {}
+    dataset['train_dataset'] = extra_dataset[32000:]
+    dataset['train_labels'] = extra_labels[32000:]
+    dataset['valid_dataset'] = extra_dataset[:32000]
+    dataset['valid_labels'] = extra_labels[:32000]
+    dataset['test_dataset'] = test_dataset
+    dataset['test_labels'] = test_labels
+    print('Dataset Built')    
+
+    return dataset
+
+def make_composite_dataset(size):
+    if (size == 'big'):
+        return big_svhn_dataset()
+    else:
+        return small_svhn_dataset()
+    
+svhn_data = make_composite_dataset('big')
 #import matplotlib.pyplot as plt
 #plt.imshow(svhn_data['train_dataset'][0])
 #print(svhn_data['train_labels'][0])
